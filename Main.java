29ca66c03b597afb3a40cfe03d382874d531097d
ttdangg <ttdang145@gmail.com>
2025-02-20 10:16:32 -0800

    problem 3 done

--- Main.java
+++ Main.java
@@ -5,6 +5,33 @@ public class Main
 {
   public static void main(String[] args)
   {
+    int[] nums = (1,2,3,4,5);
+    for (int i = 0; i< nums.length; i++)
+    {
+      System.out.println(nums[i]);
+    }
+
+
+    ArrayList<Integer> nums = new ArrayList<Integer>();
+    nums.add(new Integer(1));
+    nums.add(new Integer(2));
+    nums.add(new Integer(3));
+    nums.add(new Integer(4));
+    nums.add(new Integer(5));
+
+    for (int i = 0; i < nums.size(); i++)
+    {
+      System.out.println(nums.get(i));
+    }
+
+
+
+
+
+
+
+
+
     Scanner sc = new Scanner(System.in);
     String input = "";
 
@@ -17,12 +44,21 @@ public class Main
   // Problem 2 - HighestNum
   public static Integer highestNum(ArrayList<Integer> arr)
   {
-    return null;
+    
   }
 
   // Problem 3 - getEvens
   public static ArrayList<Integer> getEvens(final ArrayList<Integer> arr)
   {
-    return null;
+    ArrayList<Integer> ans = new ArrayList<Integer>();
+    for (int i = 0; i < arr.size(); i++)
+    {
+      Integer temp = arr.get(i);
+      if (arr.get(i).intValue() % 2 == 0)
+      {
+        ans.add(temp);
+      }
+    }
+    return ans;
   }
 }
